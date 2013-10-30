fa42ae20a5c619882b920a7bac9cc0da3dd62105
Johan Liesén <johan@liesen.se>
2013-10-30 21:56:21 +0100

    Graphviz 2.34.0

---
+++ Casks/graphviz.rb
@@ -0,0 +1,8 @@
+class Graphviz < Cask
+  url 'http://www.graphviz.org/pub/graphviz/stable/macos/lion/graphviz-2.34.0.pkg'
+  homepage 'http://www.graphviz.org/'
+  version '2.34.0'
+  sha1 'f76101d4958177bdd7d81f6728f7fd8d4edd4a1e'
+  link ''
+  install 'graphviz-2.34.0.pkg'
+end
